362ac42decfaa57f0b00f72156d422f6fe0c4442
JFPedra <jfpedrazag@unal.edu.co>
2020-11-12 23:04:25 -0500

    Navbar en construcción, falta organizar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Head from './Components/Header'
+import Navbar from './Components/Navbar'
+
 function App() {
   return (
     <div>
+      <Head />
+      <Navbar />
       <p>Aplicación React y se actualiza</p>
+
     </div>
   );
 }
